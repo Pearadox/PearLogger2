543150bb4052321c139f938d12fb6603ae239c9c
Allen Jiang <alljiang@hotmail.com>
2019-06-23 16:16:53 -0500

    add options dialog

--- GUI/ui2py.bat
+++ GUI/ui2py.bat
@@ -1,3 +1,4 @@
 pyuic5.exe -x PearLog.ui -o GUIPearLog.py
 pyuic5.exe -x AddPersonDialog.ui -o GUIAddPersonDialog.py
 pyuic5.exe -x ViewHoursDialog.ui -o GUIViewHoursDialog.py
+pyuic5.exe -x OptionsDialog.ui -o GUIOptionsDialog.py
